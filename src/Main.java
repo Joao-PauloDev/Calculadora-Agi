dad1a0c5be0e812da8df585c328e5f43abe40d53
joao.felicio <felicio.joaopaulodev@gmail.com>
2025-02-18 09:41:58 -0300

    Adiciona classe Main

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Resultado: " + Calculadora.soma(5, 8));
+        System.out.println("Resultado da subtração : " + Calculadora.subtracao(5 , 3));
+    }
+}
