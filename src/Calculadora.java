90c5ce3860946c95317d4e5a484d09d97d5bac47
joao.felicio <felicio.joaopaulodev@gmail.com>
2025-02-18 09:14:34 -0300

    Adiciona classe Calculadora

---
+++ src/Calculadora.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Calculadora {
+    public static int soma(int a, int b){
+        return a + b;
+    }
+
+
+}
